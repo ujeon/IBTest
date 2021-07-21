16830df82ea3fb9a3bca29e0808bc76488f45e65
ujeon <wj9304@gmail.com>
2021-07-21 20:35:46 +0900

    스토리보드 객체와 클래스를 연결하는 다섯 가지 방법

--- IBTest/ViewController.swift
+++ IBTest/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var uiTitle01: UILabel!
+    
+    @IBOutlet var uiTitle02: UILabel!
+    
+    @IBOutlet var uiTitle03: UILabel!
+    
+    @IBOutlet var uiTitle04: UILabel!
+    
+    @IBOutlet var uiTitle05: UILabel!
+    
+    @IBAction func clickBtn01(_ sender: Any) {
+        self.uiTitle01.text = "Button01 Clicked"
+    }
+    
+    @IBAction func clickBtn02(_ sender: Any) {
+        self.uiTitle02.text = "BUtton02 CLicked"
+    }
+    
+    @IBAction func clickBtn03(_ sender: Any) {
+        self.uiTitle03.text = "BUtton03 CLicked"
+    }
+    
+    @IBAction func clickBtn04(_ sender: Any) {
+        self.uiTitle04.text = "BUtton04 CLicked"
     }
 
-
+    @IBAction func clickBtn05(_ sender: Any) {
+        self.uiTitle05.text = "BUtton05 CLicked"
+    }
 }
 
